7c567c7f398beb4e7d243ef6c5b51679f9eb5609
lida-sokha <sokhalida1234@gmail.com>
2025-08-07 14:16:10 +0700

    homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login"; 
+import Homepage from "./pages/Homepage"; // Importing Homepage component
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Redirect root path to login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
+        {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
+        <Route path="/" element={<Homepage />} /> {/* Set Homepage as the root path */}
         {/* Routes for signup and login */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
