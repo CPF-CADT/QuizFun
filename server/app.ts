4a3d18aa4dc1596fe5bbdeba2cd737c3a285d13a
vathanakphy <vathanakphy@gmail.com>
2025-08-10 01:19:52 +0700

    implemtn game route for hosting and setup socket io

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,8 @@ import { quizzRouter } from './routes/quizz.route';
 import { errorHandler } from './middleware/errHandle.middleware';
 import { serviceRouter } from './routes/service.route';
 // import { createRandomGameHistory,createRandomGameSession,createRandomQuiz,createRandomUserData,createRandomVerificationCode } from './faker/seed';
-import { runSeed } from './faker/seed'; 
+import { runSeed } from './config/seed'; 
+import { gameRouter } from './routes/game.route';
 
 const app = express();
 
@@ -22,6 +23,8 @@ app.use('/api/user', userRouter);
 app.use('/api/quizz', quizzRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/service',serviceRouter)
+app.use('/api/game',gameRouter)
+
 app.use(errorHandler)
 runSeed();
 export default app;
