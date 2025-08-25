a32437d102b89efeadd9b9818be7c7f8860636ba
ChanpanhaCheng <chanpanhacheng@gmail.com>
2025-08-25 15:36:03 +0700

    Rate limit (#90)
    * not finish yet
    
    * finished rate-limit
    
    ---------
    
    Co-authored-by: vathanakphy <vathanakphy@gmail.com>

--- server/routes/game.route.ts
+++ server/routes/game.route.ts
@@ -4,44 +4,52 @@ import express from 'express';
 import { GameController } from '../controller/game.controller';
 import { validate } from '../middleware/validate';
 import { gameSchemas } from '../validations/game.schemas'; 
+import { globalRateLimit, quizRateLimit } from '../middleware/ratelimit.middleware';
 
 export const gameRouter = express.Router();
 
 // Apply validation middleware to each route
 gameRouter.get('/', 
+    globalRateLimit,
     validate(gameSchemas.getSessions), 
     GameController.getSessions
 );
 
 gameRouter.get('/:id', 
+    globalRateLimit,
     validate(gameSchemas.idParam), 
     GameController.getSessionDetails
 );
 
 gameRouter.get('/:id/history', 
+    globalRateLimit,
     validate(gameSchemas.idParam), 
     GameController.getSessionHistory
 );
 
 gameRouter.post('/:sessionId/feedback', 
+    globalRateLimit,
     validate(gameSchemas.sessionIdParam),
     validate(gameSchemas.addFeedback),   
     GameController.addFeedbackToSession
 );
 
 gameRouter.get('/:sessionId/results', 
+    quizRateLimit,
     validate(gameSchemas.sessionIdParam),
     validate(gameSchemas.getSessionResults),
     GameController.getSessionResults
 );
 
-gameRouter.get('/:sessionId/performance/guest', 
+gameRouter.get('/:sessionId/performance/guest',
+    quizRateLimit,
     validate(gameSchemas.sessionIdParam),
     validate(gameSchemas.getGuestPerformance),
     GameController.getGuestPerformanceInSession
 );
 
 gameRouter.get('/:sessionId/performance/:userId', 
+    quizRateLimit,
     validate(gameSchemas.userPerformanceParams), 
     GameController.getUserPerformanceInSession
 );
--- server/routes/report.route.ts
+++ server/routes/report.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { ReportController } from "../controller/report.controller";
-
+import { authenticateToken } from "../middleware/authenicate.middleware";
+import { globalRateLimit } from '../middleware/ratelimit.middleware';
 export const reportRouter = express.Router();
 
 // Routes for reporting and analytics
-reportRouter.get("/my-quizzes",ReportController.getMyQuizzesForReport);
-reportRouter.get("/quiz/:quizId",ReportController.getQuizAnalytics);
-reportRouter.get("/activity-feed",ReportController.getUserActivityFeed);
+reportRouter.get("/my-quizzes",globalRateLimit, authenticateToken,ReportController.getMyQuizzesForReport);
+reportRouter.get("/quiz/:quizId",globalRateLimit, authenticateToken,ReportController.getQuizAnalytics);
+reportRouter.get("/activity-feed",globalRateLimit, authenticateToken,ReportController.getUserActivityFeed);
 reportRouter.get("/quiz/:quizId/feedback",ReportController.getQuizFeedback);
