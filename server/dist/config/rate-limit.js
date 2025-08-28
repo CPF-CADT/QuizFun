41cb19e850d2c68a343c9cae5615939319524701
vathanakphy <vathanakphy@gmail.com>
2025-08-28 14:51:46 +0700

    sdfsd

--- server/dist/config/rate-limit.js
+++ server/dist/config/rate-limit.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ratelimitConfig = void 0;
 exports.ratelimitConfig = {
-    global: { windowMs: 15 * 60 * 1000, max: 100, message: "Too many requests, please try again later." },
-    quiz: { windowMs: 15 * 60 * 1000, max: 15, message: "Too many requests, please try again later." },
+    global: { windowMs: 15 * 60 * 1000, max: 200, message: "Too many requests, please try again later." },
+    quiz: { windowMs: 15 * 60 * 1000, max: 150, message: "Too many requests, please try again later." },
 };
